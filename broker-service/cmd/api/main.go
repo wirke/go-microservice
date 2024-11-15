47a4c50739528a292aab9270f568494e4375443e
Petar Virijević <wiriyevich@gmail.com>
2024-11-15 09:28:11 +0100

    fixing docker configuration
    go formatting

--- broker-service/cmd/api/main.go
+++ broker-service/cmd/api/main.go
@@ -8,20 +8,18 @@ import (
 
 const webPort = "80"
 
-type Config struct {}
+type Config struct{}
 
 func main() {
 	app := Config{}
 
 	log.Printf("Starting broker service on port %s\n", webPort)
 
-	// define http server
 	srv := &http.Server{
-		Addr: fmt.Sprintf(":%s", webPort),
+		Addr:    fmt.Sprintf(":%s", webPort),
 		Handler: app.routes(),
 	}
 
-	// start the server
 	err := srv.ListenAndServe()
 	if err != nil {
 		log.Panic(err)
